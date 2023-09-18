ccd9e819ef2cd4659da3f678703b60e39aae4027
WagnerCoding <102889180+WagnerCoding@users.noreply.github.com>
2023-09-18 10:09:54 -0600

    progress made with file archiving utility

--- Python Source Code/FIleArchiving.py
+++ Python Source Code/FIleArchiving.py
@@ -6,6 +6,11 @@ import lzma
 import gzip
 import shutils
 import zipfile
+import io
+
+
+
+file_types = [("All files (*.*)", "*.*")]
 
 def main():
 
@@ -13,7 +18,9 @@ def main():
     layout = [
         #[sg.Image(key="-IMAGE-")],
         [
-
+            [sg.Text("File to hash: "), sg.Input(size=(25, 1), key="-FILE-"), sg.FileBrowse(file_types=file_types)],
+            [sg.HSeparator()],
+            [sg.Button("Hash that File!"), sg.Button("Hash that text!"), sg.Button("Verify that Hash!"), sg.Button("Exit")],
             [sg.HSeparator()],
             [sg.Text('Your Hashes: ')],
             sg.Output(size=(150, 30)),
@@ -25,6 +32,14 @@ def main():
 
         if event == "Exit" or event == sg.WIN_CLOSED:
             break
+
+        if event == "Hash that File!":
+            filename = values["-FILE-"]
+            archivename = filename + ".gz"
+
+            if os.path.exists(filename):
+                with open(filename, 'rb') as src, gzip.open(archivename, 'wb') as dst:
+                    dst.writelines(src)
 
     window.close()
 
